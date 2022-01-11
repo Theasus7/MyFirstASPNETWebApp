a730ba1bae70f2aac5be402f5fb65656fe76bbd2
Theasus7 <anderson.taylor98@gmail.com>
2022-01-11 15:48:05 -0700

    Add project files.

---
+++ WebApplication1/wwwroot/js/FanMail.js
@@ -0,0 +1,9 @@
+﻿document.getElementById("btnSend").addEventListener("click", function () {
+    alert("From: " + document.getElementById("txtFrom").value +
+        ", Subject: " + document.getElementById("txtSubject").value +
+        ", Message: " + document.getElementById("txtMessage").value + "\n\n Slash Approves");
+
+
+    $("#picBand").fadeToggle("slow");
+
+})
